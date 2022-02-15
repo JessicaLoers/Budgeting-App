52c275bfcbd2e720d1098b74166fc3a499cbc481
Philipp Kaiser <p.kaiser95@gmx.de>
2022-02-15 11:57:45 +0100

    Added progress bar

--- components/BudgetCard.tsx
+++ components/BudgetCard.tsx
@@ -1,19 +1,25 @@
-import Button from "./Button"
+import Button from "./Button";
 
 function BudgetCard() {
-    return (
-        <div className="container rounded-2xl p-4 shadow-xl ring-1">
-            <div className="flex justify-between items-center">
-            <h2 className="text-3xl font-bold">Entertainment</h2>
-            <div><span className="text-2xl">$200</span>/ $1000</div>
-            </div>
-            <div>Progress bar</div>
-            <div className="flex justify-end items-center">
-            <Button primary>Add expense</Button>
-            <Button>View expense</Button>
-            </div>
+  return (
+    <div className='container rounded-2xl p-4 shadow-xl ring-1'>
+      <div className='flex justify-between items-center'>
+        <h2 className='text-3xl font-bold'>Entertainment</h2>
+        <div>
+          <span className='text-2xl'>$200</span>/ $1000
         </div>
-    )
+      </div>
+      <div className='w-full bg-violet-200 rounded-full h-4 my-8'>
+        <div
+          className='bg-violet-500 h-4 rounded-full'
+          style={{ width: "45%" }}></div>
+      </div>
+      <div className='flex justify-end items-center'>
+        <Button primary>Add expense</Button>
+        <Button>View expense</Button>
+      </div>
+    </div>
+  );
 }
 
-export default BudgetCard
+export default BudgetCard;
