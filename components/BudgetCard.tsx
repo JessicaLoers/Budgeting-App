f4a96f9cebb4329168446fdc3fdef6767953acea
Philipp Kaiser <p.kaiser95@gmx.de>
2022-02-16 19:59:24 +0100

    Added dynamic budget-card-data

--- components/BudgetCard.tsx
+++ components/BudgetCard.tsx
@@ -1,26 +1,38 @@
-import { MouseEventHandler } from 'react';
-import Button from './Button';
+import { MouseEventHandler } from "react";
+import stateManagement from "../lib/stateManagement";
+import Button from "./Button";
 
 interface Props {
   onShowAddExpense: MouseEventHandler;
+  categoryBudget: {
+    individualExpenses: any[];
+    category: string;
+    budget: number;
+    overSpend: boolean;
+  };
 }
 
-function BudgetCard({ onShowAddExpense }: Props) {
+function BudgetCard({ onShowAddExpense, categoryBudget }: Props) {
   return (
-    <div className="rounded-2xl p-4 shadow-xl ring-1 mt-12">
-      <div className="flex justify-between items-center">
-        <h2 className="text-3xl font-bold">Entertainment</h2>
+    <div className='rounded-2xl p-4 shadow-xl ring-1 mt-12'>
+      <div className='flex justify-between items-center'>
+        <h2 className='text-3xl font-bold'>{categoryBudget.category}</h2>
         <div>
-          <span className="text-2xl">$200</span>/ $1000
+          <span className='text-2xl'>
+            {categoryBudget.individualExpenses.reduce(
+              (a, b) => a + b.expense,
+              0
+            )}
+          </span>
+          / {categoryBudget.budget}
         </div>
       </div>
-      <div className="w-full bg-violet-200 rounded-full h-4 my-8">
+      <div className='w-full bg-violet-200 rounded-full h-4 my-8'>
         <div
-          className="bg-violet-500 h-4 rounded-full"
-          style={{ width: '45%' }}
-        ></div>
+          className='bg-violet-500 h-4 rounded-full'
+          style={{ width: "45%" }}></div>
       </div>
-      <div className="flex justify-end items-center">
+      <div className='flex justify-end items-center'>
         <Button primary onClickFunction={onShowAddExpense}>
           Add expense
         </Button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
-import type { NextPage } from 'next';
-import { useState } from 'react';
-import Header from '../components/Header';
-import BudgetCard from '../components/BudgetCard';
-import UncategorizedBudgetCard from '../components/UncategorizedBudgetCard';
-import TotalBudgetCard from '../components/TotalBudgetCard';
-import AddBudget from '../components/AddBudget';
-import AddExpense from '../components/AddExpense';
+import type { NextPage } from "next";
+import { useState } from "react";
+import Header from "../components/Header";
+import BudgetCard from "../components/BudgetCard";
+import UncategorizedBudgetCard from "../components/UncategorizedBudgetCard";
+import TotalBudgetCard from "../components/TotalBudgetCard";
+import AddBudget from "../components/AddBudget";
+import AddExpense from "../components/AddExpense";
+import stateManagement from "../lib/stateManagement";
 
 const Home: NextPage = () => {
   const [showAddBudget, setShowAddBudget] = useState(false);
   const [showAddExpense, setShowAddExpense] = useState(false);
+
+  const storage = stateManagement();
 
   function handleShowAddBudget() {
     setShowAddBudget(!showAddBudget);
@@ -20,12 +23,17 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="container mx-auto">
+    <div className='container mx-auto'>
       <Header
         onShowAddBudget={handleShowAddBudget}
         onShowAddExpense={handleShowAddExpense}
       />
-      <BudgetCard onShowAddExpense={handleShowAddExpense} />
+      {storage.map((expense) => (
+        <BudgetCard
+          onShowAddExpense={handleShowAddExpense}
+          categoryBudget={expense}
+        />
+      ))}
       <UncategorizedBudgetCard onShowAddExpense={handleShowAddExpense} />
       <TotalBudgetCard onShowAddExpense={handleShowAddExpense} />
       {showAddBudget && <AddBudget onCloseAddBudget={handleShowAddBudget} />}
